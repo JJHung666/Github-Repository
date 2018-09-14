9f5fc6f0f19fdcee95db3563721816e33bd14d6d
Justin Lin <jlin21@choate.edu>
2018-09-14 10:51:48 -0400

    9/14/2018 WP

---
+++ 9:14:2018_WP.py
@@ -0,0 +1,13 @@
+# Date: 9/14/2018
+# Descriptions: This program is a class warmup
+# source: none
+
+import math as m
+import sys
+P = eval(sys.argv[1])
+r = eval(sys.argv[2])
+t = eval(sys.argv[3])
+print(P*(m.exp(1))**(r*t))
+
+
+# notes: <, <=, >, >=, != not equal, == equal, 
