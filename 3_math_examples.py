4bf29cad5bdf8e4e4446bc4c623ffaff9b911cc9
Justin Lin <jlin21@choate.edu>
2018-09-13 23:14:49 -0400

    failed + notes

---
+++ 3_math_examples.py
@@ -0,0 +1,4 @@
+# Date: 9/13/2018
+# Descriptions: This program was written with what was learned in class
+# Source: ms.Healey
+
---
+++ Variable_notes.py
@@ -0,0 +1,7 @@
+# note
+# date: 9/10/2018
+
+first = 1 # integer
+second = 3.14 # float
+third = 'a' # character
+fourth = 'test' # string
