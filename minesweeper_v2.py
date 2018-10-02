932f263708a29297575a5c142e5f835aff5eb4f2
Justin Lin <jlin21@choate.edu>
2018-10-02 15:26:23 -0400

    minesweeper

--- minesweeper_v2.py
+++ minesweeper_v2.py
@@ -9,34 +9,40 @@ import math
 w = int(input("input the width please ")) + 2
 h = int(input("input the height please ")) + 2
 
+bomb_count = 0
+
+
 grid_size = [[0]*w for x in range(h)]
 for x in range(1,len(grid_size)-1):
-	for y in range(1, len(grid_size)-2):
+	for y in range(1, len(grid_size)-3):
 		b = r.randint(1,h-2)
 		c = r.randint(1,w-2)
 		grid_size[b][c] = "æ"
+
 		if grid_size[(b-1)][(c-1)] != "æ":
 			grid_size[(b-1)][(c-1)] += 1
-		if grid_size[(b-1)][(c)] != "æ":
+		elif grid_size[(b-1)][(c)] != "æ":
 			grid_size[(b-1)][(c)] += 1
-		if grid_size[(b-1)][(c+1)] != "æ":
+		elif grid_size[(b-1)][(c+1)] != "æ":
 			grid_size[(b-1)][(c+1)] += 1
-		if grid_size[(b)][(c+1)] != "æ":
+		elif grid_size[(b)][(c+1)] != "æ":
 			grid_size[(b)][(c+1)] += 1
-		if grid_size[(b)][(c-1)] != "æ":
+		elif grid_size[(b)][(c-1)] != "æ":
 			grid_size[(b)][(c-1)] += 1
-		if grid_size[(b+1)][(c-1)] != "æ":
+		elif grid_size[(b+1)][(c-1)] != "æ":
 			grid_size[(b+1)][(c-1)] += 1
-		if grid_size[(b+1)][(c)] != "æ":
+		elif grid_size[(b+1)][(c)] != "æ":
 			grid_size[(b+1)][(c)] += 1
-		if grid_size[(b+1)][(c+1)] != "æ":
+		elif grid_size[(b+1)][(c+1)] != "æ":
 			grid_size[(b+1)][(c+1)] += 1
 
-bomb_count = 0
 
 
 for x in range(1,len(grid_size)-1):
 	for y in range (1, len(grid_size[0])-1):
-		print(grid_size[x][y],end=" \n")
+		if grid_size[x][y] == "æ":
+			bomb_count += 1
+		print(grid_size[x][y],end=" ")
+	print("")
 
 print("there are", bomb_count, "bombs")
