4a7f0d8a2d268399ebaac80508a391c75c906b52
Justin Lin <jlin21@choate.edu>
2018-10-24 23:30:08 -0400

    success

--- Julia_Set.py
+++ Julia_Set.py
@@ -1,40 +1,41 @@
 # Name: Justin Lin
 # Date: 10/17/2018
-# Description: Julia set
-# Source(s): googled what the julia set was
+# Description: Julia
+# Source(s): 
 
-# For each pixel (x, y) on the screen, do:
-import random as r
 from PIL import Image as i
+import random as r
 
-x = 0
-y = 0
+xa, xb = -1.5, 1.5
+ya, yb = -1.5, 1.5
 
-tries = 0
-max_tries = 256
+img_sizex = 1000
+img_sizey = 1000
 
-while (x*x + y*y < 4  and  tries < max_tries) 
-    xtemp = zx*zx - zy*zy
 
-    zy = 2*zx*zy  + cy 
+pixeled = []
 
-    zx = xtemp + cx
-    
-    tries = tries + 1 
-  
-    if (tries == max_tries)
-        return a = (0, 0, 0)
-    else
-        return tries
-        for i in range(maxtries):
+maxtries = 256
+
+image = i.new("RGB", (img_sizex, img_sizex))
+
+for y in range(img_sizey):
+	yz = y * (yb-ya)/(img_sizey-1) + ya
+	for x in range(img_sizex):
+		xz = x * (xb-xa)/(img_sizex-1) + xa
+		z = complex(xz, yz)
+		c = complex( -0.334,0.626)
+		for i in range(maxtries):
 			if abs(z) >= 2.0:
 				break
-    r = 256//i
-	g = i//255
-	b = i
-
-
-
-image.putpixel((x,y),(r,g,b))
-
-image.show("Julia_Set.png", "PNG")
+			z = z**2 + c
+		r = i
+		g = i
+		b = i
+		if i < 200:
+			image.putpixel((x,y),(r, g, b))
+		else:
+			image.putpixel((x,y), (x//3,y//3,(x*y)//7))
+		
+image.show("Julia", "PNG")
+image.save("Julia.png", "PNG")
---
+++ Mandlebrot_Julia_Project.py
@@ -0,0 +1,130 @@
+# Name: Justin Lin
+# Date: 10/17/2018
+# Description: Mandlebrot
+# Source(s): 
+
+# from PIL import Image as i
+	
+def mandlebrot_1():
+	from PIL import Image as i
+	xa, xb = -0.3111, -0.2790
+	ya, yb = -0.6723, -0.6402
+
+	img_sizex = 1000
+	img_sizey = 1000
+
+	counts = 10
+	pixeled = []
+
+	maxtries = 256
+	a = 0
+	b = 0
+	c = 0
+
+	image = i.new("RGB", (img_sizex, img_sizex))
+
+	for y in range(img_sizey):
+		yc = y * (yb-ya)/(img_sizey-1) + ya
+		for x in range(img_sizex):
+			xc = x * (xb-xa)/(img_sizex-1) + xa
+			c = complex(xc, yc)
+			z = 0
+			for i in range(maxtries):
+				if abs(z) >= 2.0:
+					break
+				z = z**2 + c
+
+			r = 256-i
+			b = i+256
+			if b >= 256:
+				b = b - i
+			g = 256-i
+			image.putpixel((x,y),(r, g, b))
+			if i < 100:
+				image.putpixel((x,y), (x,y,x-y))
+	image.show("Mandlebrot_1", "PNG")
+	image.save("Mandlebrot_1.png", "PNG")
+
+def mandlebrot_2():
+	from PIL import Image as i
+	xa, xb = -0.9084, -0.9103
+	ya, yb = 0.2396, 0.2415
+
+	img_sizex = 1000
+	img_sizey = 1000
+
+	counts = 10
+	pixeled = []
+
+	maxtries = 256
+	a = 0
+	b = 0
+	c = 0
+
+	image = i.new("RGB", (img_sizex, img_sizex))
+
+	for y in range(img_sizey):
+		yc = y * (yb-ya)/(img_sizey-1) + ya
+		for x in range(img_sizex):
+			xc = x * (xb-xa)/(img_sizex-1) + xa
+			c = complex(xc, yc)
+			z = 0
+			for i in range(maxtries):
+				if abs(z) >= 2.0:
+					break
+				z = z**2 + c
+
+			r = 256-i
+			b = i+256
+			if b >= 256:
+				b = b - i
+			g = 256-i
+			image.putpixel((x,y),(r, g, b))
+			if i < 200:
+				image.putpixel((x,y), (y,x,y-x))
+	image.show("Mandlebrot_2", "PNG")
+	image.save("Mandlebrot_2.png", "PNG")
+
+def julia():
+	from PIL import Image as i
+	import random as r
+
+	xa, xb = -1.5, 1.5
+	ya, yb = -1.5, 1.5
+
+	img_sizex = 1000
+	img_sizey = 1000
+
+
+	pixeled = []
+
+	maxtries = 256
+
+	image = i.new("RGB", (img_sizex, img_sizex))
+
+	for y in range(img_sizey):
+		yz = y * (yb-ya)/(img_sizey-1) + ya
+		for x in range(img_sizex):
+			xz = x * (xb-xa)/(img_sizex-1) + xa
+			z = complex(xz, yz)
+			c = complex( -0.334,0.626)
+			for i in range(maxtries):
+				if abs(z) >= 2.0:
+					break
+				z = z**2 + c
+			r = i
+			g = i
+			b = i
+			if i < 200:
+				image.putpixel((x,y),(r, g, b))
+			else:
+				image.putpixel((x,y), (x//3,y//3,(x*y)//7))
+			
+	image.show("Julia", "PNG")
+	image.save("Julia.png", "PNG")
+
+mandlebrot_1()
+
+mandlebrot_2()
+
+julia()
